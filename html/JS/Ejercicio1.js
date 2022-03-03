a273f5fb5be146b4025b7634fd80046054d1d8eb
CAROLGISEL <gisselabonilla@hotmail.com>
2022-03-03 11:35:52 -0500

    subir actividad

--- html/JS/Ejercicio1.js
+++ html/JS/Ejercicio1.js
@@ -1,15 +1,3 @@
-/*1. En un almacén  se están ofreciendo las siguientes ofertas  de descuento en compras  (ver tabla). 
-Calcular el valor de descuento y valor a pagar por la compra de cierta cantidad de un articulo , 
-si se conoce el precio unitario 
-
-
-
-Desde	Hasta	%Descuento
-0	     100	2
-100.01	 250	5
-250.01	 800	8
-8001.01	 más	12*/
-
 alert('En un almacén  se están ofreciendo las siguientes ofertas  de descuento en compras  ver tabla Calcular el valor de descuento y valor a pagar por la compra de cierta cantidad de un articulo ,\n si se conoce el precio unitario;\n \n  TABLA DE DESCUENT0   \n \nUNIDADES - DESCUENTO\n0 - 100             2%,\n100.01 - 250     5%,\n250.01-800       8%,\n8001.01-inf       12%');
 document.getElementById('tabla').innerHTML = 'En un almacén  se están ofreciendo las siguientes ofertas  de descuento en compras  ver tabla Calcular el valor de descuento y valor a pagar por la compra de cierta cantidad de un articulo ,<br> si se conoce el precio unitario;<br><br>  TABLA DE DESCUENT0   <br><br>UNIDADES - DESCUENTO<br>0 - 100__________2%,<br>100.01 - 250_____5%,<br>250.01-800______8%,<br>8001.01-inf______12%';
 let nombre = prompt('ingrese nombre del producto');
